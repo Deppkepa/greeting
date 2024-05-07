6ca1e1ae528f1609affc80173b1ed6b10339ecc1
Deppkepa <149363955+Deppkepa@users.noreply.github.com>
2024-05-07 21:47:04 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='setup_test_hello',
+   version='1.0',
+   description='Тестовая функция привествия пользователя по имени для изучения setup.',
+   license='Apache-2.0 license',
+   author='Deppkepa',
+   author_email='dasha.sisimirova@bk.ru',
+   url='https://github.com/Deppkepa/greeting.git',
+   packages=find_packages(),
+   install_requires=['pytest','numpy'],
+   extras_require={
+        'test': [
+            'pytest'
+        ],
+   },
+   python_requires='>=0',
+)
