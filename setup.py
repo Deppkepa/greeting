060d923aee42bc21df56e319f1cdf7f13b5a0208
Deppkepa <149363955+Deppkepa@users.noreply.github.com>
2024-05-07 21:51:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='setup_test_hello',
    version='1.0',
    description='Тестовая функция привествия пользователя по имени для изучения setup.',
-   license='Apache-2.0 license',
+   license='CC0-1.0 license',
    author='Deppkepa',
    author_email='dasha.sisimirova@bk.ru',
    url='https://github.com/Deppkepa/greeting.git',
